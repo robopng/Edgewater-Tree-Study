b27915e279b7804ee67785479817f4071c902c4e
robopng <109000271+robopng@users.noreply.github.com>
2024-03-20 18:55:21 -0500

    initial commit

--- venv/refactor.py
+++ venv/refactor.py
@@ -1,4 +1,4 @@
-import sys
+import os
 
 # address specific fields
 ADDR_NUM = ''
@@ -33,46 +33,51 @@ grf_col = 5
 age_col = 6
 species_col = 3
 note_col = 1
-with open(f'./src/{name}.csv',mode='r') as input:
-    # skip the header
-    input.readline()
-    with open(f'./src/{name}_sorted.csv',mode='w') as output:
-        output.write('ADDR_NUM,ADDR_DIR,ADDR_NAME,TRACT,BLOCK,'
-                     + 'CIRCUMFERENCE,SPECIES,DIAMETER,GRF,AGE,NOTE\n')
-        for line in input:
-            vals = line.rstrip().split(',')
-            # comment out as appropriate
-            addr = (vals[addr_col]).split(' ')
-            ADDR_NUM = addr[addr_num_col]
-            ADDR_DIR = addr[addr_dir_col]
-            ADDR_NAME = addr[addr_name_col]
-            BLOCK = ''
-            TRACT = ''
-            addr = f'{ADDR_NUM} {ADDR_DIR} {ADDR_NAME}'
-            if addr not in address_tracker:
-                address_tracker[addr] = 0
-            address_tracker[addr] += 1
 
-            DIAMETER = vals[diam_col]
-            CIRCUMFERENCE = vals[cir_col]
-            GRF = vals[grf_col]
-            AGE = vals[age_col]
-            SPECIES = vals[species_col]
-            if SPECIES not in species_tracker:
-                species_tracker[SPECIES] = 0
-            species_tracker[SPECIES] += 1
+for name in os.listdir('./src/inputs'):
+    with open(f'./src/inputs/{name}.csv',mode='r') as input:
+        # skip the header
+        input.readline()
+        with open(f'./src/outputs/{name}_sorted.csv',mode='w') as output:
+            output.write('ADDR_NUM,ADDR_DIR,ADDR_NAME,TRACT,BLOCK,'
+                         + 'CIRCUMFERENCE,SPECIES,DIAMETER,GRF,AGE,NOTE\n')
+            for line in input:
+                # rstrip mainly for trailing newline
+                vals = line.rstrip().split(',')
+                # address specific fields
+                addr = (vals[addr_col]).split(' ')
+                ADDR_NUM = addr[addr_num_col]
+                ADDR_DIR = addr[addr_dir_col]
+                ADDR_NAME = addr[addr_name_col]
+                BLOCK = ''
+                TRACT = ''
+                addr = f'{ADDR_NUM} {ADDR_DIR} {ADDR_NAME}'
+                if addr not in address_tracker:
+                    address_tracker[addr] = 0
+                address_tracker[addr] += 1
+                # tree specific fields
+                DIAMETER = vals[diam_col]
+                CIRCUMFERENCE = vals[cir_col]
+                GRF = vals[grf_col]
+                AGE = vals[age_col]
+                SPECIES = vals[species_col]
+                if SPECIES not in species_tracker:
+                    species_tracker[SPECIES] = 0
+                species_tracker[SPECIES] += 1
+                # misc fields
+                NOTE = vals[note_col] if vals[note_col] != '' else ' '
+                if int(AGE) >= 100: over_100 += 1
+                total += 1
 
-            NOTE = vals[note_col] if vals[note_col] != '' else ' '
+                output.write(f'{ADDR_NUM},{ADDR_DIR},{ADDR_NAME},{TRACT},{BLOCK},{CIRCUMFERENCE}'
+                             + f',{SPECIES},{DIAMETER},{GRF},{AGE},{NOTE}\n')
+            output.write('\n')
 
-            if int(AGE) >= 100: over_100 += 1
-            total += 1
-            output.write(f'{ADDR_NUM},{ADDR_DIR},{ADDR_NAME},{TRACT},{BLOCK},{CIRCUMFERENCE}'
-                         + f',{SPECIES},{DIAMETER},{GRF},{AGE},{NOTE}\n')
-        output.write('\n')
-
-with open(f'./src/tracks.csv',mode='w') as file:
+with open(f'./src/outputs/tracks.csv',mode='w') as file:
     for dictionary in (block_tracker, tract_tracker, address_tracker, species_tracker):
         for key in dictionary:
             file.write(f'{key}:{dictionary[key]}\n')
         file.write('\n')
+    file.write(f'{over_100}\n')
+    file.write(f'{total}\n')
     file.write('\n')
