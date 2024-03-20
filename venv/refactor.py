c075b7bd8aa8bafe392032adc8af8b1a6058fa90
robopng <109000271+robopng@users.noreply.github.com>
2024-03-20 15:07:41 -0500

    initial commit

--- venv/refactor.py
+++ venv/refactor.py
@@ -1,9 +1,18 @@
 import sys
 
 # address specific fields
+# tracked: addr, tract, block
 ADDR, TRACT, BLOCK = {},{},{}
 ADDR_NUM, ADDR_DIR, ADDR_NAME = '','',''
 # tree specific fields
+# tracked: species
 DIAMETER, CIRCUMFERENCE, AGE, SPECIES, GRF = {},{},{},{},{}
 # misc fields
 NOTE = {}
+
+name = 'EGA (example)'
+with open(f'./src/{name}.csv',mode='r') as file:
+    pass
+
+with open(f'./src/tracks.csv',mode='x') as file:
+    pass
