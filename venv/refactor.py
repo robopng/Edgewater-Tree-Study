4968e906f32a351ae947e3a6c197e97df4e2b343
robopng <109000271+robopng@users.noreply.github.com>
2024-03-20 16:39:57 -0500

    pseudocode for when I get back home

--- venv/refactor.py
+++ venv/refactor.py
@@ -1,18 +1,57 @@
 import sys
 
 # address specific fields
-# tracked: addr, tract, block
-ADDR, TRACT, BLOCK = {},{},{}
-ADDR_NUM, ADDR_DIR, ADDR_NAME = '','',''
-# tree specific fields
-# tracked: species
-DIAMETER, CIRCUMFERENCE, AGE, SPECIES, GRF = {},{},{},{},{}
-# misc fields
-NOTE = {}
+ADDR_NUM = ''
+ADDR_DIR = ''
+ADDR_NAME = ''
+block_tracker = {}
+tract_tracker = {}
+address_tracker = {}
 
+# tree specific fields
+DIAMETER = 0.0
+CIRCUMFERENCE = 0.0
+GRF = 0.0 # I don't actually know what GRF is.
+AGE = 0
+species_tracker = {}
+
+# misc fields
+NOTE = ''
+over_100 = 0
+total = 0
+
+# slightly annoying but it really is easiest to just do this manually
+# per excel tab
 name = 'EGA (example)'
+addr_num_col = 0
+addr_dir_col = 0
+addr_name_col = 0
+diam_col = 0
+circ_col = 0
+grf_col = 0
+age_col = 0
+species_col = 0
+note_col = 0
 with open(f'./src/{name}.csv',mode='r') as file:
-    pass
+    for line in file:
+        vals = [line.split(',')]
+        # address handling
+        # block lookup
+        # tract lookup
+        address_tracker[f'{ADDR_NUM} {ADDR_DIR} {ADDR_NAME}'] += 1
+        DIAMETER = vals[diam_col]
+        CIRCUMFERENCE = vals[cir_col]
+        GRF = vals[grf_col]
+        AGE = vals[age_col]
+        species_tracker[vals[species_col]] += 1
+        NOTE = vals[note_col]
+
+        if AGE >= 100: over_100 += 1
+        total += 1
 
 with open(f'./src/tracks.csv',mode='x') as file:
-    pass
+    for dictionary in (block_tracker, tract_tracker, address_tracker, species_tracker):
+        for key in dictionary:
+            fike.write(key)
+        file.write('\n')
+    file.write('\n')
