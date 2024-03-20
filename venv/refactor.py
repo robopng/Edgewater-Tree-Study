5e1b28905c0da9f3c1e4541103d3c3d1ecda1f84
robopng <109000271+robopng@users.noreply.github.com>
2024-03-20 14:55:57 -0500

    initial commit

---
+++ venv/refactor.py
@@ -0,0 +1,9 @@
+import sys
+
+# address specific fields
+ADDR, TRACT, BLOCK = {},{},{}
+ADDR_NUM, ADDR_DIR, ADDR_NAME = '','',''
+# tree specific fields
+DIAMETER, CIRCUMFERENCE, AGE, SPECIES, GRF = {},{},{},{},{}
+# misc fields
+NOTE = {}
