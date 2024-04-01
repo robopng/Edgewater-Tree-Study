771a2afa15a5f95de923697c3eb328eefe089931
robopng <109000271+robopng@users.noreply.github.com>
2024-03-31 21:42:24 -0500

    getting addresses

---
+++ venv/lookup.py
@@ -0,0 +1,19 @@
+# read 9 times
+
+path = ".\\src\\inputs\\Copy of Edgewater Heritage Tree Data - MASTER.csv"
+path_out = ".\\src\\outputs\\addresses.csv"
+counter = 1
+
+with (open(path) as file, open(path_out,mode='w') as out):
+    for line in file:
+        line = line.split(',')
+        line = f'{line[0]} {line[1]} {line[2]}'
+        if "BROKEN!" in line or len(line) < 6 or line.upper() != line:
+            line='26 E PEARSON'
+        line = f'{counter},{line},Chicago,IL,60660\n'
+        out.write(line)
+        counter += 1
+
+
+# curl --form benchmark=Public_AR_Current --form address="4600 Silver Hill Rd, Washington, DC 20233" https://geocoding.geo.census.gov/geocoder/locations/onlineaddress?parameters
+# curl --form addressFile=@addresses.csv --form benchmark=Public_AR_Current https://geocoding.geo.census.gov/geocoder/locations/addressbatch --output geocoderesult.csv
